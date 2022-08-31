84dcaa2deb4869e8cf41952b0c2bf0bd3bf47f64
togramort <togramort@gmail.com>
2022-08-31 20:06:01 +0300

    unit testing

---
+++ testing/MinApp/TestMinApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestMinApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("TestMinApp")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d4eceae5-c9e6-4934-a9ff-d98028f0e7f4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ testing/MinApp/TestMinApp/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinApp;
+
+namespace TestMinApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provide
+        ///information about and functionality for the current test run.
+        ///</summary>
+        
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        public void TestMin()
+        {
+            int min;
+            min = Program.Min(3, 4, 5);
+            Assert.AreEqual(3, min);
+        }
+    }
+}
