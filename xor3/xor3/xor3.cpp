4f8f448c774c22536216e9e2db22a27dea759fb7
togramort <togramort@gmail.com>
2022-08-31 21:41:59 +0300

    labs

---
+++ xor3/xor3/xor3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int main() {
+
+    unsigned int key;
+    std::cout << "key " << "\n";
+    std::cin >> key;
+
+    int n;
+    std::cout << "enter size of the array " << "\n";
+    std::cin >> n;
+
+    char* a = new char[n];
+    std::cout << "array: ";
+    for (int i = 0; i < n; ++i) {
+        std::cin >> a[i];
+    }
+
+    char enc, dec;
+    std::cout << "encrypted: ";
+    for (int i = 0; i < n; ++i) {
+        enc = a[i] ^ key;
+        a[i] = enc;
+        std::cout << enc << " ";
+    }
+    std::cout << "\n";
+    std::cout << "decrypted: ";
+    for (int i = 0; i < n; ++i) {
+        dec = a[i] ^ key;
+        std::cout <<  dec << " ";
+    }
+    delete[] a;
+    return 0;
+}
